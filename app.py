a5d06d0f54639579c5c443c6b5364a5724301142
jimmykimani <jimmykkimani@gmail.com>
2017-01-25 23:11:17 +0300

    Comments on each line to show what the code does

--- app.py
+++ app.py
@@ -3,15 +3,33 @@ import click
 from views import Lyrics
 
 class AppRun(cmd.Cmd):
+    '''Name of the program'''
     prompt = "Welcome to Discovr>>"
 
     def do_find(self, query):
+        '''
+        The do_find commmand  enables a user to view a list of songs
+        based on the query ie Find drake
+        ''''
         if(query == ""):
             print('"Sorry, You did not input a Song name or Artist Name"')
         else:
              lyric.song_find(query)
 
     def do_view(self, query):
+        '''
+        This command enables a user to view a list of sings based
+        onthe Track id where by the user inputs the id of the trackto view
+        Lyrics
+        '''
+        lyric.song_view(query)
+
+    def do_view(self, query):
+        '''
+        This is for saving the lyrics of a certain song based on its id
+        to the local database
+
+        '''
         lyric.song_view(query)
 
 
@@ -19,6 +37,7 @@ class AppRun(cmd.Cmd):
         click.clear()
 
     def do_q(self):
+        ''' The command is used to exit and perform another action'''
         # Exit
         return True
 
